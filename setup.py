079be5326a5011866677beddace00666eaad85f9
LIUHENG6650 <44800878+LIUHENG6650@users.noreply.github.com>
2020-02-21 19:34:19 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='yolov3_tf2',
+      url='https://github.com/zzh8829/yolov3-tf2',
+      author='liuheng',
+      author_email='2644078712@qq.com / cystyle.g@gmail.com',
+      packages=['yolov3_tf2'])
